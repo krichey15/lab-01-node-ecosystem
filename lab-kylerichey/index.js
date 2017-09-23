1a488b7e37b467a4275fb6304a4ac75a37a6bd73
Kyle Richey <richey.kyle.i@gmail.com>
2017-09-23 16:35:18 -0700

    math functions

--- lab-kylerichey/index.js
+++ lab-kylerichey/index.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const greet = require(__dirname + '/lib/greet.js');
+const arithmetic = require(__dirname + '/lib/arithmetic.js');
 
 console.log(greet('Kyle'));
-console.log(greet(1));
+console.log(greet());
+console.log(arithmetic.add(2, 2));
+console.log(arithmetic.subtract(4, 1));
--- lab-kylerichey/lib/arithmetic.js
+++ lab-kylerichey/lib/arithmetic.js
@@ -1,1 +1,11 @@
 'use strict';
+
+let arithmetic = module.exports = {};
+
+arithmetic.add = function (num1, num2) {
+  return num1 + num2;
+};
+
+arithmetic.subtract = function (num1, num2) {
+  return num1 - num2;
+};
