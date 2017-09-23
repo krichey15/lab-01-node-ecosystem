989458b94a0f6b01b67764c259aa6e7beeace073
Kyle Richey <richey.kyle.i@gmail.com>
2017-09-23 15:13:25 -0700

    greet test

--- lab-kylerichey/test/greet.test.js
+++ lab-kylerichey/test/greet.test.js
@@ -2,3 +2,21 @@
 
 const expect = require('expect');
 const greet = require(__dirname + '/lib/greet.js');
+
+describe('greet', function (){
+
+  it('returns null when no name is given', function (){
+
+    let result = greet();
+    expect(result).toBe(null);
+
+  });
+
+  it('will greet with name or word that was given', function (){
+
+    let result = greet('Kyle');
+    expect(result).toBe('Hello Kyle');
+
+  });
+
+});
