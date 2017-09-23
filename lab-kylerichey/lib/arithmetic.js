bbdcdfc764f6128c480271cd2c4d9698ea7dfb50
Kyle Richey <richey.kyle.i@gmail.com>
2017-09-23 16:48:57 -0700

    more math

--- lab-kylerichey/lib/arithmetic.js
+++ lab-kylerichey/lib/arithmetic.js
@@ -3,9 +3,17 @@
 let arithmetic = module.exports = {};
 
 arithmetic.add = function (num1, num2) {
-  return num1 + num2;
+  if (typeof num1 || typeof num2 === 'number'){
+    return num1 + num2;
+  } else {
+    return null;
+  }
 };
 
 arithmetic.subtract = function (num1, num2) {
-  return num1 - num2;
+  if (typeof num1 || typeof num2 == 'number'){
+    return num1 - num2;
+  } else {
+    return null;
+  }
 };
